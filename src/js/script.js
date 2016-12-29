02d28e3f105986aaa751788861ce89e33068e847
5tka <ferol2010@yandex.ru>
2016-12-29 09:20:47 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+console.log('hello from script.js');
+$( document ).ready(function() {
+
+    $('.search-label').click(function(){
+
+        $('.search-wrap').toggleClass('vv');
+        $('.search-input').toggleClass('w320');
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // конец document.ready
