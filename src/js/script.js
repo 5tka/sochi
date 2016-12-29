106db5a772c0641f9a190573fb2b6f0169aad9c9
xeonalex <lux1995@yandex.ru>
2016-12-29 18:23:38 +0200

    main section

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,28 @@
 console.log('hello from script.js');
-$( document ).ready(function() {
+$(document).ready(function() {
 
-    $('.search-label').click(function(){
+    // $('.search-label').click(function(){
 
-        $('.search-wrap').toggleClass('vv');
-        $('.search-input').toggleClass('w320');
+    //     $('.search-wrap').toggleClass('vv');
+    //     $('.search-input').toggleClass('w320');
+    // });
 
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // слайдер на главной
+	$('.partners-slider').slick({
+	  infinite: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  // variableWidth: true,
+	  arrows: false
+	});
+	$('.partners-prev').click(function (e){
+		e.preventDefault();
+		$('.partners-slider').slick('slickPrev');
+		return false;
+	});
+	$('.partners-next').click(function (e){
+		e.preventDefault();
+		 $('.partners-slider').slick('slickNext');
+		 return false;
+	});
 }); // конец document.ready
